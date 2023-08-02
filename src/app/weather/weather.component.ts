71c8b9fbdeac29129f59a905b391a1550973f70c
p-ajinkya <132088591+p-ajinkya@users.noreply.github.com>
2023-08-02 10:18:24 +0530

    remove static data
    remove static data

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -10,52 +10,7 @@ import { WeatherService } from '../weather.service';
 export class WeatherComponent {
   public weatherSearchForm!: FormGroup;
   constructor(private formBuilder: FormBuilder, private weatherService: WeatherService) {}
-  public weatherData: any = {
-    "coord": {
-        "lon": 73.8553,
-        "lat": 18.5196
-    },
-    "weather": [
-        {
-            "id": 804,
-            "main": "Clouds",
-            "description": "overcast clouds",
-            "icon": "04d"
-        }
-    ],
-    "base": "stations",
-    "main": {
-        "temp": 299.34,
-        "feels_like": 299.34,
-        "temp_min": 299.34,
-        "temp_max": 299.34,
-        "pressure": 1009,
-        "humidity": 70,
-        "sea_level": 1009,
-        "grnd_level": 948
-    },
-    "visibility": 10000,
-    "wind": {
-        "speed": 6.3,
-        "deg": 254,
-        "gust": 11.95
-    },
-    "clouds": {
-        "all": 100
-    },
-    "dt": 1690950813,
-    "sys": {
-        "type": 2,
-        "id": 2083365,
-        "country": "IN",
-        "sunrise": 1690936937,
-        "sunset": 1690983562
-    },
-    "timezone": 19800,
-    "id": 1259229,
-    "name": "Pune",
-    "cod": 200
-};
+  public weatherData: any = null;
   public currentDate = new Date();
   public error: any = null;
   ngOnInit() {
